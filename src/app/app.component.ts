f798ed0ed5e446234f6a84cd0825fefe71bc7fac
IvanaHac <vodickovaivana96@email.cz>
2026-01-09 13:12:20 +0100

    Change footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { HomeHeroComponent } from "./home-hero/home-hero.component";
-import { FooterComponent } from "./footer/footer.component";
 import { AboutUsComponent } from "./about-us/about-us";
 import {ServicesComponent} from "./services/services";
 import {ContactComponent} from "./contact/contact";
@@ -11,7 +10,7 @@ import {InquiryFormComponent} from "./inquiry-form/inquiry-form";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HomeHeroComponent, FooterComponent, AboutUsComponent, ServicesComponent, ContactComponent],
+  imports: [HeaderComponent, HomeHeroComponent, AboutUsComponent, ServicesComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
